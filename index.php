b5f40ef469b8f4bfa724ffe240451817d6f93f08
Matthew <you@example.com>
2019-05-28 16:41:02 +0800

    sample php update

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-Maintenanace in progress...
+<?php
 
-Please visit again later.
+// Comment these lines to hide errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require 'includes/config.php';
+require 'includes/functions.php';
+
+init();
